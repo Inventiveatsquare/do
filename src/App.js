5ae7bc9ba7207fdb695a95887d1a4cb9e69c65bf
Hardeep Singh <inventiveatsquare@gmail.com>
2020-07-30 02:12:45 +0530

    adas

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import  './css/style.css'
+class App extends Component {
+render(){
+  return(
+    <h1>Welcome to react clients test</h1>
+  )
+}
+}
+
+export default App;
+
